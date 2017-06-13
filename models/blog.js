4f1c20bad40331d9fe98a02fbe3e3b6752e04398
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2017-06-13 16:51:15 +0800

    add blog

--- models/blog.js
+++ models/blog.js
@@ -2,11 +2,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var blogSchema = new Schema({
     title:String,
+    content:String,
     author:String,
     date:Date,
-    time:String,
-    read_count:Integer,
+    read_count:Number,
     author_id:String,
 });
-
-exports.blogs = mongoose.model('blogs', blogSchema);
+blog_model = mongoose.model('blogs', blogSchema);
+blog_model.createBlog = function(blog_info, req, res){
+    var blog = new blog_model({
+        title:blog_info.title || "",
+        content:blog_info.content || "",
+        author:blog_info.author || "",
+        date:new Date(),
+        read_count:0,
+        author_id:blog_info.author_id || "",
+    });
+    blog.save(function(err){
+        if(err){
+            res.send("error");
+        }else{
+            res.send("success");
+        }
+    });
+};
+exports.blogs = blog_model
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var user = require('../models/user').user;
+var blog = require('../models/blog').blogs;
 var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -53,5 +54,20 @@ router.get('/logout',function(req, res){
 router.get('/index',function(req, res){
     res.render('personal_index', {title:'个人主页',user:JSON.stringify(req.session.user)});
 });
-
+router.get('/personal_add_blog',function(req, res){
+    res.render('personal_add_blog', {title:'新建Blog',user:JSON.stringify(req.session.user)});
+});
+router.post('/personal_add_blog',function(req, res){
+    if(req.session.user){
+        var blog_info = {
+            title:req.body.title,
+            content:req.body.content,
+            author:req.session.user.username,
+            author_id:req.session.user._id,
+        };
+        blog.createBlog(blog_info, req, res);
+    }else{
+        res.send("error");
+    }
+});
 module.exports = router;
