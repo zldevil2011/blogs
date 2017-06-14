d6abb8366cc9d91b57db11d58d871b37ea4dfe83
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2017-06-14 11:53:42 +0800

    remove login check for index&information template

--- routes/index_route.js
+++ routes/index_route.js
@@ -11,11 +11,11 @@ router.get('/', function(req, res) {
   if(req.session.user){
     classification.all_classification(function(err, result){
       if(err){
-        res.render('index', { title: 'Express' , user:JSON.stringify(req.session.user), classification_list:"", blog_list:""});
+        res.render('index', { title: 'Express' , user_inf_tag:1,user:JSON.stringify(req.session.user), classification_list:"", blog_list:""});
       }else{
         blog.all_blog(function(err, blog_list_result){
           if(err){
-            res.render('index', { title: 'Express' , user:JSON.stringify(req.session.user), classification_list:JSON.stringify(result),blog_list:"" });
+            res.render('index', { title: 'Express' , user_inf_tag:1,user:JSON.stringify(req.session.user), classification_list:JSON.stringify(result),blog_list:"" });
           }else{
             var ep = new eventproxy();
             ep.after('author_portrait', blog_list_result.length, function(portraits){
@@ -37,7 +37,7 @@ router.get('/', function(req, res) {
               }
               blog_list_result = new_blog_list_result;
               console.log(blog_list_result);
-              res.render('index', { title: 'Express' , user:JSON.stringify(req.session.user), classification_list:JSON.stringify(result),blog_list:JSON.stringify(blog_list_result) });
+              res.render('index', { title: 'Express' , user_inf_tag:1,user:JSON.stringify(req.session.user), classification_list:JSON.stringify(result),blog_list:JSON.stringify(blog_list_result) });
             });
             var portrait = [];
             for(var i = 0; i < blog_list_result.length; ++i){
@@ -56,7 +56,52 @@ router.get('/', function(req, res) {
     });
 
   }else{
-    res.redirect('/users/login/');
+    // res.redirect('/users/login/');
+    classification.all_classification(function(err, result){
+      if(err){
+        res.render('index', { title: 'Express' , user_inf_tag:0,user:JSON.stringify({}), classification_list:"", blog_list:""});
+      }else{
+        blog.all_blog(function(err, blog_list_result){
+          if(err){
+            res.render('index', { title: 'Express' , user_inf_tag:0, user:JSON.stringify({}), classification_list:JSON.stringify(result),blog_list:"" });
+          }else{
+            var ep = new eventproxy();
+            ep.after('author_portrait', blog_list_result.length, function(portraits){
+              var new_blog_list_result = [];
+              for(var i = 0; i < blog_list_result.length; ++i) {
+                var c = blog_list_result[i];
+                var t = {
+                  "_id":c._id,
+                  "title":c.title,
+                  "content":c.content,
+                  "author":c.author,
+                  "date":c.date,
+                  "read_count":c.read_count,
+                  "author_id":c.author_id,
+                  "test":"test",
+                  "author_portrait":portraits[i],
+                };
+                new_blog_list_result.push(t);
+              }
+              blog_list_result = new_blog_list_result;
+              console.log(blog_list_result);
+              res.render('index', { title: 'Express' ,user_inf_tag:0, user:JSON.stringify({}), classification_list:JSON.stringify(result),blog_list:JSON.stringify(blog_list_result) });
+            });
+            var portrait = [];
+            for(var i = 0; i < blog_list_result.length; ++i){
+                var c = blog_list_result[i];
+                user.user_information(c.author_id, function(err, user_info){
+                  if(err){
+                    ep.emit("author_portrait", "");
+                  }else{
+                    ep.emit("author_portrait", user_info[0].portrait);
+                  }
+                });
+            }
+          }
+        });
+      }
+    });
   }
 
 });
@@ -65,9 +110,9 @@ router.get('/blog/:blog_id', function(req, res) {
     console.log(result);
     console.log(err);
     if(err){
-      res.render('blog_information', { title: 'Blog' , user:JSON.stringify({}), blog:JSON.stringify({}) });
+      res.render('blog_information', { title: 'Blog' , user_inf_tag:0,user:JSON.stringify({}), blog:JSON.stringify({}) });
     }else{
-      res.render('blog_information', { title: 'Blog' , user:JSON.stringify({}), blog:JSON.stringify(result[0]), });
+      res.render('blog_information', { title: 'Blog' , user_inf_tag:0,user:JSON.stringify({}), blog:JSON.stringify(result[0]), });
     }
   })
 
